224d701bd4ffe2dea5f2352ee980f9415ed78043
LeeJiWon <dlwldnjs427@naver.com>
2024-09-19 23:04:11 +0900

    :wrench: feat : User 엔티티 및 레포지토리 파일 생성

---
+++ src/main/java/com/finance/user/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.finance.user.repository;
+
+import com.finance.user.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface UserRepository extends JpaRepository<User, UUID> {
+}
