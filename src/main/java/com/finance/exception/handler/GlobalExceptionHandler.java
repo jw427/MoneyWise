9c47f05ca6460be03b3b8a3aa1a37ac203de1e5a
LeeJiWon <dlwldnjs427@naver.com>
2024-09-19 20:33:58 +0900

    :wrench: init : 에러 헨들링 세팅

---
+++ src/main/java/com/finance/exception/handler/GlobalExceptionHandler.java
@@ -0,0 +1,17 @@
+package com.finance.exception.handler;
+
+import com.finance.exception.BadRequestException;
+import com.finance.exception.ErrorResponse;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(BadRequestException.class)
+    public ResponseEntity<ErrorResponse> handleBadRequestException(BadRequestException e) {
+        return ResponseEntity.badRequest()
+                .body(new ErrorResponse(e.getErrorCode(), e.getErrorCode().getMessage()));
+    }
+}
