ef0717b8105c26139592b981f681ee39e594cf33
LeeJiWon <dlwldnjs427@naver.com>
2024-09-21 01:02:41 +0900

    :wrench: feat : category service와 controller 생성

---
+++ src/main/java/com/finance/category/controller/CategoryController.java
@@ -0,0 +1,13 @@
+package com.finance.category.controller;
+
+import com.finance.category.service.CategoryService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/categories")
+@RequiredArgsConstructor
+public class CategoryController {
+    private final CategoryService categoryService;
+}
---
+++ src/main/java/com/finance/category/service/CategoryService.java
@@ -0,0 +1,11 @@
+package com.finance.category.service;
+
+import com.finance.category.repository.CategoryRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CategoryService {
+    private final CategoryRepository categoryRepository;
+}
