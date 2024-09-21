1391e002c4d8a556921497145c34dc1b04b14403
LeeJiWon <dlwldnjs427@naver.com>
2024-09-21 15:47:46 +0900

    :sparkles: feat : 카테고리 전체 조회 controller 작성

--- src/main/java/com/finance/category/controller/CategoryController.java
+++ src/main/java/com/finance/category/controller/CategoryController.java
@@ -1,5 +1,6 @@
 package com.finance.category.controller;
 
+import com.finance.category.dto.CategoryListResponseDto;
 import com.finance.category.dto.CreateCategoryRequestDto;
 import com.finance.category.dto.CreateCategoryResponseDto;
 import com.finance.category.service.CategoryService;
@@ -8,6 +9,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/categories")
@@ -20,5 +23,12 @@ public class CategoryController {
     public ResponseEntity<CreateCategoryResponseDto> createCategory(@RequestHeader(value = "Authorization")String token, @Valid @RequestBody CreateCategoryRequestDto requestDto) {
         CreateCategoryResponseDto responseDto = categoryService.createCategory(token, requestDto);
         return ResponseEntity.status(HttpStatus.CREATED).body(responseDto);
+    }
+
+    // 카테고리 전체 조회
+    @GetMapping
+    public ResponseEntity<List<CategoryListResponseDto>> getCategoryList(@RequestHeader(value = "Authorization")String token) {
+        List<CategoryListResponseDto> responseDto = categoryService.getCategoryList(token);
+        return ResponseEntity.ok().body(responseDto);
     }
 }
