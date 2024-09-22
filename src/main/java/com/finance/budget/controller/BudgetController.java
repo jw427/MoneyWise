e52e94717c0432d59033a6d912112e659c0c12e5
LeeJiWon <dlwldnjs427@naver.com>
2024-09-23 01:20:32 +0900

    :sparkles: feat : 예산 설정 controller 작성

--- src/main/java/com/finance/budget/controller/BudgetController.java
+++ src/main/java/com/finance/budget/controller/BudgetController.java
@@ -1,13 +1,24 @@
 package com.finance.budget.controller;
 
+import com.finance.budget.dto.CreateBudgetRequestDto;
+import com.finance.budget.dto.CreateBudgetResponseDto;
 import com.finance.budget.service.BudgetService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/budgets")
 @RequiredArgsConstructor
 public class BudgetController {
     private final BudgetService budgetService;
+
+    // 예산 설정
+    @PostMapping
+    public ResponseEntity<CreateBudgetResponseDto> createBudget(@RequestHeader(value = "Authorization") String token, @Valid @RequestBody CreateBudgetRequestDto requestDto) {
+        CreateBudgetResponseDto responseDto = budgetService.createBudget(token, requestDto);
+        return ResponseEntity.status(HttpStatus.CREATED).body(responseDto);
+    }
 }
