bb301f6a2fa877d6cbdcd36435a9c7b06b61afe0
LeeJiWon <dlwldnjs427@naver.com>
2024-09-24 01:37:40 +0900

    :wrench: feat : 지출 service 생성

---
+++ src/main/java/com/finance/expense/service/ExpenseService.java
@@ -0,0 +1,11 @@
+package com.finance.expense.service;
+
+import com.finance.expense.repository.ExpenseRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class ExpenseService {
+    private final ExpenseRepository expenseRepository;
+}
