4b02f3713e8855342d20b78c2cd8f078bef85bbd
LeeJiWon <dlwldnjs427@naver.com>
2024-09-24 02:25:50 +0900

    :sparkles: feat : 지출 생성 controller 작성

--- src/main/java/com/finance/expense/controller/ExpenseController.java
+++ src/main/java/com/finance/expense/controller/ExpenseController.java
@@ -1,13 +1,24 @@
 package com.finance.expense.controller;
 
+import com.finance.expense.dto.CreateExpenseRequestDto;
+import com.finance.expense.dto.CreateExpenseResponseDto;
 import com.finance.expense.service.ExpenseService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/expenses")
 @RequiredArgsConstructor
 public class ExpenseController {
     private final ExpenseService expenseService;
+
+    // 지출 생성
+    @PostMapping
+    public ResponseEntity<CreateExpenseResponseDto> createExpense(@RequestHeader(value = "Authorization") String token, @Valid @RequestBody CreateExpenseRequestDto requestDto) {
+        CreateExpenseResponseDto responseDto = expenseService.createExpense(token, requestDto);
+        return ResponseEntity.status(HttpStatus.CREATED).body(responseDto);
+    }
 }
